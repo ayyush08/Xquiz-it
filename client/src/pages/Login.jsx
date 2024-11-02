efd71e0a8634c2e6b890e589876c25442998ac21
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2024-11-02 22:57:04 +0530

    Add react-hook-form and react-icons; implement login form with validation

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,13 +1,69 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { Button } from '../components/ui/button';
 const Login = () => {
+  const {
+    register,
+    isSubmitting,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+};
+  const onSubmit = async (data) => {
+    console.log(data)
+  }
   return (
     <>
-    <div className='mx-auto'>
-      <h1 className='text-4xl font-mono text-white font-bold mb-10 m-4'>Login</h1>
-      
-        
-    </div>
+      <div className='mx-auto p-8'>
+        <h1 className='text-4xl text-center font-sans italic text-white font-bold mb-10 m-4'>Login</h1>
+        <form className='w-1/3 mx-auto space-y-4' onSubmit={handleSubmit(onSubmit)}>
+          <div className=''>
+            <label className="block text-white text-xl font-mono">Email: </label>
+            <input
+              placeholder='Enter your email'
+              type="email"
+              {...register("email", { required: true})}
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-cyan-500"
+            />
+            {errors.email && <div className="text-red-500">Email is required</div>}
+          </div>
+          <div className="relative">
+                        <label className="block text-white font-mono text-xl">Password:</label>
+                        <input
+                            type={passwordVisible ? 'text' : 'password'}
+                            {...register("password", {
+                                required: { value: true, message: 'Password is required' },
+                                minLength: { value: 8, message: 'Password must be at least 8 characters' },
+                                maxLength: { value: 20, message: 'Password cannot exceed 20 characters' }
+                            })}
+                            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-cyan-500 "
+                        />
+                        {errors.password && <div className="text-red-500">{errors.password.message}</div>}
+                        <span
+                            onClick={togglePasswordVisibility}
+                            className="absolute top-[40px] right-0 pr-3 flex items-center cursor-pointer"
+                        >
+                            {passwordVisible ? <FaEyeSlash style={{color:'black'}} /> : <FaEye style={{color:'black'}} />}
+                        </span>
+                    </div>
+                    <div className='flex items-center justify-center'>
+
+                    <button
+                        type="submit"
+                        className=" mx-auto text-white bg-orange-700 text-xl px-4 font-mono py-3 rounded-lg hover:bg-orange-800 "
+                        >
+                        {isSubmitting ? 'Logging in...' : 'Login'}
+                    </button>
+                      </div>
+        </form>
+
+      </div>
     </>
   )
 }
