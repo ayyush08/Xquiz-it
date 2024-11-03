c5c18ee9756b6e375fbaaf1aa364f8450360f96f
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2024-11-03 06:11:39 +0530

    Server folder and connection setups

---
+++ server/src/index.js
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv'
+import connectDB from './db/index.js'
+import { app } from './app.js'
+
+dotenv.config(
+    {
+        path:'./.env'
+    }
+)
+
+connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("ERRR:",error);
+    })
+
+    app.get('/',(req,res)=>{
+        res.send("Server is running")
+    })
+
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server listening on port ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.error("MongoDB connection Error: ",error)
+})
