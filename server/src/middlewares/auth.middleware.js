86a222aa62c755a1694a853579dadd8b0b98dd82
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2024-11-10 22:48:41 +0530

    Improve error message for missing JWT in auth middleware

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -8,9 +8,9 @@ import jwt from 'jsonwebtoken';
 export const verifyJWT = asyncHandler(async (req, res, next) => {
     try {
         const token = req.cookies?.accessToken || req.header('Authorization')?.replace('Bearer ', '')
-
+        console.log(token);
         if (!token) {
-            throw new ApiError(401, 'Unauthorized request')
+            throw new ApiError(401, 'Unauthorized request, token not found')
         }
 
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
